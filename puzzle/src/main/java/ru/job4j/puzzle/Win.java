36bbc7b4cc1b016082bbd9339a027be4caebadb4
yungdanie <nikoches253@gmail.com>
2022-02-28 14:39:55 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,36 +2,33 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        return (monoHorizontal(board) || monoVertical(board));
-    }
-
-    public static boolean monoHorizontal(int[][] board) {
-        boolean result = true;
-        for (int row = 0; row < board.length; row++) {
-            result = true;
-            for (int cell = 0; cell < board[row].length; cell++) {
-                if (board[row][cell] != 1) {
-                    result = false;
-                    break;
-                } else if (cell == board[row].length - 1) {
-                    return true;
-                }
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                result = true;
+                break;
             }
         }
         return result;
     }
 
-    public static boolean monoVertical(int[][] board) {
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int cell = 0; cell < board[row].length; cell++) {
+            if (board[row][cell] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(int[][] board, int column) {
         boolean result = true;
         for (int row = 0; row < board.length; row++) {
-            result = true;
-            for (int cell = 0; cell < board[row].length; cell++) {
-                if (board[cell][row] != 1) {
-                    result = false;
-                    break;
-                } else if (cell == board[row].length - 1) {
-                    return true;
-                }
+            if (board[row][column] != 1) {
+                result = false;
+                break;
             }
         }
         return result;
